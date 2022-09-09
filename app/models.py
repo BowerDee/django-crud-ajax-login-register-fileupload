caa725bd688dcff82f320394e03f24b576d390a9
sk.lv <778324184@qq.com>
2022-09-09 14:25:46 +0800

    update

--- app/models.py
+++ app/models.py
@@ -10,13 +10,15 @@ class VerfyModel(models.Model):
 
 class UserInfo(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
-    phone = models.CharField(max_length=16) # 手机号码
+    #phone = models.CharField(max_length=16) # 手机号码
     createdate = models.DateTimeField(auto_now_add = True) # 创建时间
     updatedate = models.DateTimeField(auto_now = True) # 创建时间
-    username = models.CharField(max_length=16) # 用户名称
+    username = models.CharField(max_length=16) # 三方平台名称
+    #third_name = models.CharField(max_length=16) # 三方平台名称
+    third_id = models.CharField(max_length=16) # 三方平台ID
     USERNAME_FIELD ='id' 
     password = models.CharField(max_length=16) # pwd
-    avator_url = models.CharField(max_length=255) # 头像链接
+    avatar_url = models.CharField(max_length=255) # 头像链接
 
 class RoleInfo(models.Model):
     id = models.AutoField(primary_key=True)
@@ -46,10 +48,10 @@ class Question(models.Model):
     tips = models.CharField(max_length=255) # 提示
     qtype = models.IntegerField() # 问题类型
     stage = models.IntegerField() # 所属关卡
-    skip = models.BooleanField() # 是否启用
+    skip = models.BooleanField() # 是否可以跳过
     score = models.IntegerField() # 分数
-    an_1 = models.CharField(max_length=255) # 选项1
-    an_2 = models.CharField(max_length=255) # 选项2
-    an_3 = models.CharField(max_length=255) # 选项3
-    an_4 = models.CharField(max_length=255) # 选项4
+    op_1 = models.CharField(max_length=255) # 选项1
+    op_2 = models.CharField(max_length=255) # 选项2
+    op_3 = models.CharField(max_length=255) # 选项3
+    op_4 = models.CharField(max_length=255) # 选项4
     correct = models.CharField(max_length=16) # 正确答案
