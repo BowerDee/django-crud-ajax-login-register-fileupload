5a86910337a2c757ff2197bb0a03f562ed90b5fc
sk.lv <778324184@qq.com>
2022-09-14 14:45:24 +0800

    update

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class AccountInfo(AbstractBaseUser):
     username = models.CharField(max_length=16) # 三方平台名称
     #third_name = models.CharField(max_length=16) # 三方平台名称
     third_id = models.CharField(max_length=16) # 三方平台ID
-    USERNAME_FIELD ='id' 
+    USERNAME_FIELD ='id'
     password = models.CharField(max_length=16) # pwd
     avatar_url = models.CharField(max_length=255, null=True, blank=True) # 头像链接
     country = models.CharField(max_length=32, null=True, blank=True) # 国家
@@ -28,17 +28,23 @@ class AccountInfo(AbstractBaseUser):
 class RoleInfo(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.ForeignKey('AccountInfo', on_delete=models.CASCADE)
-    last_game_kind = models.IntegerField(null=True, blank=True) # 最后一次玩的游戏
-    last_stage = models.IntegerField(null=True, blank=True) # 最后一次玩的关卡
-    last_step = models.IntegerField(null=True, blank=True) # 关卡题目index
+    # last_dynasty = models.IntegerField(null=True, blank=True) # 最后一次玩的游戏
+    # last_stage = models.IntegerField(null=True, blank=True) # 最后一次玩的模式
+    # last_level = models.IntegerField(null=True, blank=True) # 关卡index
     updatedate = models.DateTimeField(auto_now = True)
+    nike_name = models.CharField(max_length=64, null=True, blank=True) # 头部资源
+    header_res = models.CharField(max_length=64, null=True, blank=True) # 头部资源
+    body_res = models.CharField(max_length=64, null=True, blank=True) # 身体资源
+    max_score = models.IntegerField(null=True, blank=True)
 
 # 分数记录
 class Score(models.Model):
     id = models.AutoField(primary_key=True)
-    roleid = models.ForeignKey('RoleInfo', on_delete=models.CASCADE)
-    stage = models.IntegerField() # 关卡
-    step_id = models.IntegerField(null=True, blank=True) # 此关卡当前的 step ID
+    roleid = models.IntegerField()#models.ForeignKey('RoleInfo', on_delete=models.CASCADE)
+    # dynasty = models.CharField(max_length=32, null=True, blank=True) # 朝代
+    # stage = models.IntegerField() # 模式
+    level = models.IntegerField() # 关卡
+    #step_id = models.IntegerField(null=True, blank=True) # 此关卡当前的 step ID
     score = models.IntegerField(null=True, blank=True) # 关卡得分
 
 # 公告
@@ -49,10 +55,25 @@ class Brand(models.Model):
     enable = models.BooleanField(null=True, blank=True) # 是否启用
     createdate = models.DateTimeField(auto_now = True, null=True, blank=True) # 创建时间
 
-# 游戏种类
-class GameKind(models.Model):
+# 游戏朝代
+class Dynasty(models.Model):
     id = models.AutoField(primary_key=True)
     desc = models.CharField(max_length=64, null=True, blank=True) # 描述
+
+# 游戏小关卡
+class Stage(models.Model):
+    id = models.AutoField(primary_key=True)
+    #stage = models.IntegerField() #models.ForeignKey('StageMode')
+    dynasty = models.IntegerField() # models.ForeignKey('GameKind')
+    desc = models.CharField(max_length=64, null=True, blank=True) # 描述
+    # score = models.IntegerField(null=True, blank=True) # 拼图游戏没有小分，一个图就是一关，所以分数写在这里
+    # uncorrect_score = models.IntegerField(null=True, blank=True) # 答错分数
+
+# 小关卡内的游戏等级列表
+class Level(models.Model):
+    id = models.AutoField(primary_key=True)
+    stage = models.IntegerField()
+    score = models.IntegerField(null=True, blank=True) # 分数
 
 # 游戏小关 种类 枚举类
 class StepMode(models.Model):
@@ -60,27 +81,18 @@ class StepMode(models.Model):
     mode = models.IntegerField(null=True, blank=True) # 0 : 默认初始化  # 1 答题模式 # 2 拼图模式
     desc = models.CharField(max_length=64, null=True, blank=True) # 描述
 
-# 游戏小关卡
-class Stage(models.Model):
-    id = models.AutoField(primary_key=True)
-    #stage = models.IntegerField() #models.ForeignKey('StageMode')
-    game_kind = models.IntegerField() # models.ForeignKey('GameKind')
-    desc = models.CharField(max_length=64, null=True, blank=True) # 描述
-    # score = models.IntegerField(null=True, blank=True) # 拼图游戏没有小分，一个图就是一关，所以分数写在这里
-    # uncorrect_score = models.IntegerField(null=True, blank=True) # 答错分数
-
-# 每一个 stage 中的 step 可能有多个也可能有一个
+# 每一个 Level 中的 step 可能有多个也可能有一个
 class Step(models.Model):
     id = models.AutoField(primary_key=True)
-    stage = models.IntegerField() # 所属关卡 models.ForeignKey('StageSetting')
+    level = models.IntegerField() # 所属关卡 models.ForeignKey('StageSetting')
     step_mode = models.IntegerField() #models.ForeignKey('StageMode')
     topic = models.CharField(max_length=128,null=True, blank=True) # 标题
     describtion = models.CharField(max_length=255,null=True, blank=True) # 描述
     tips = models.CharField(max_length=255, null=True, blank=True) # 提示
     qtype = models.IntegerField(null=True, blank=True) # 问题类型
     is_skip = models.BooleanField(null=True, blank=True) # 是否可以跳过
-    correct_score = models.IntegerField(null=True, blank=True) # 分数
-    uncorrect_score = models.IntegerField(null=True, blank=True) # 答错分数
+    #correct_score = models.IntegerField(null=True, blank=True) # 分数
+    #uncorrect_score = models.IntegerField(null=True, blank=True) # 答错分数
     op_1 = models.CharField(max_length=255,null=True, blank=True) # 选项1
     op_2 = models.CharField(max_length=255,null=True, blank=True) # 选项2
     op_3 = models.CharField(max_length=255,null=True, blank=True) # 选项3
