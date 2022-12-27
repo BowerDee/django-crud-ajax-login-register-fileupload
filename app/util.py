1fbb0cb131b66bf4fa0cc1699fa42c4675e55c08
rust.lyu <rustlyu@163.com>
2022-12-27 10:50:42 +0800

    update

--- app/util.py
+++ app/util.py
@@ -12,31 +12,49 @@ from datetime import datetime, timedelta
 from django import utils 
 
 def getScoreSum(dynasty):
-    level = []
-    print(dynasty)
+    #level = []
+    #print(dynasty)
+    #if dynasty == "han":
+    #    for i in range(1, 21):
+    #        level.append(i)
+    #else:
+    #    for i in range(21, 41):
+    #        level.append(i)
+    #print(level)
+    #roleIDlist = Score.objects.filter(level__in=level).values('roleid').distinct()
+    #ret = []
+    #for r in roleIDlist:
+    #    rr = Score.objects.filter(roleid=r['roleid'], level__in=level).aggregate(Sum('score'))['score__sum']
+    #    role = RoleInfo.objects.filter(id=r['roleid']).first()
+    #    if role is None:
+    #        continue
+    #    roleStr = serializer(role,exclude_attr='userid')
+    #    if role.userid.third_id is not None:
+    #        url = "https://slbpark.liangzimodel.com:8084/resource/avatar/{id}.png".format(id=role.userid.third_id)
+    #        roleStr.update({'avatar_url':url})
+    #    roleStr.update({'score':rr})
+    #    roleStr.update({'role_id':r['roleid']})
+    #    ret.append(roleStr)
+    #ret = sorted(ret, key=lambda item:item['score'], reverse=True)#[0:10]
+    #print(ret)
+    #return ret
+    ret = None
     if dynasty == "han":
-        for i in range(1, 21):
-            level.append(i)
+        ret = RoleInfo.objects.filter(max_score_han__gt = 0).order_by('-max_score_han')[:10]
     else:
-        for i in range(21, 41):
-            level.append(i)
-    print(level)
-    roleIDlist = Score.objects.filter(level__in=level).values('roleid').distinct()
-    ret = []
-    for r in roleIDlist:
-        rr = Score.objects.filter(roleid=r['roleid'], level__in=level).aggregate(Sum('score'))['score__sum']
-        role = RoleInfo.objects.filter(id=r['roleid']).first()
-        if role is None:
-            continue
-        roleStr = serializer(role,exclude_attr='userid')
+        ret = RoleInfo.objects.filter(max_score_tang__gt = 0).order_by('-max_score_tang')[:10]
+    for role in ret:
         if role.userid.third_id is not None:
             url = "https://slbpark.liangzimodel.com:8084/resource/avatar/{id}.png".format(id=role.userid.third_id)
-            roleStr.update({'avatar_url':url})
-        roleStr.update({'score':rr})
-        roleStr.update({'role_id':r['roleid']})
-        ret.append(roleStr)
-    ret = sorted(ret, key=lambda item:item['score'], reverse=True)#[0:10]
-    print(ret)
+            role.avatar_url = url
+            role.role_id = role.id
+            if dynasty == "han":
+                role.score = role.max_score_han
+            else:
+                role.score = role.max_score_tang
+            #role.update({'avatar_url':url})
+    #ret = sorted(ret, key=lambda item:item['score'], reverse=True)#[0:10]
+    #print(ret)
     return ret
 
 def getUserCount_day():
